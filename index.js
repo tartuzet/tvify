cdc1e4c8adcadf6653663349d0d5f3022da07027
Gustavo Isidoro <tartuzet@gmail.com>
2017-01-16 16:36:38 -0600

    clase 5

--- index.js
+++ index.js
@@ -1,40 +1,66 @@
 $(function () {
+var $container = $('#app-body').find('.tv-shows');
+var template = '<article class="tv-show">'+
+'<div class="left img-container">'+
+'<img src=":img:" alt=":img alt:">'+
+'</div>'+
+'<div class="right info">'+
+'<h1>:name:</h1>'+
+'<p>:summary:</p>'+
+'</div>'+
+'</article>';
 
+function renderShows(shows){
+  shows.forEach(function(show){
+    console.log(show);
+    var article=template
+    .replace(':name:',show.name)
+    .replace(':img:',show.image.medium)
+    .replace(':summary:',show.summary)
+    .replace(':img alt:',show.name+" Logo");
+
+    var $article = $(article);
+    $article.hide();
+    $container.append($article);
+    $article.show(2000);
+
+  })
+}
 //Submit search form
 
 
-  $('#app-body').find('form')
-  .submit(function (ev){
-    ev.preventDefault();
-    var busqueda= $(this)
-    .find('input[type="text"]')
-    .val();
+$('#app-body').find('form')
+.submit(function (ev){
+  ev.preventDefault();
+  var busqueda= $(this)
+  .find('input[type="text"]')
+  .val();
 
+  $container.find('.tv-show').remove();
+  $loader = $('<div class="loader">');
+  $loader.appendTo($container);
+  $.ajax({
+    url:'http://api.tvmaze.com/search/shows',
+    data:{q:busqueda},
+    success: function(res,textStatus,xhr){
+      $loader.remove();
+      var shows = res.map(function (el) {
+        return el.show;
+      })
+      renderShows(shows);
+
+
+    }
   })
-var template = '<article class="tv-show">'+
-          '<div class="left img-container">'+
-            '<img src=":img:" alt=":img alt:">'+
-            '</div>'+
-          '<div class="right info">'+
-          '<h1>:name:</h1>'+
-            '<p>:summary:</p>'+
-          '</div>'+
-        '</article>';
+
+})
+
 
 $.ajax({
   url:'http://api.tvmaze.com/shows',
-  success: function(data,textStatus,xhr){
-    var $container = $('#app-body').find('.tv-shows');
-    data.forEach(function(show){
-      var article=template
-      .replace(':name:',show.name)
-      .replace(':img:',show.image.medium)
-      .replace(':summary:',show.summary)
-      .replace(':img alt:',show.name+" Logo");
-
-      $container.append($(article))
-
-    })
+  success: function(shows,textStatus,xhr){
+    $container.find('.loader').remove();
+    renderShows(shows);
   }
 })
 
