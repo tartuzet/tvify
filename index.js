d3087ffd6ccf644c1d57b0f69d70fca755e69abd
Gustavo Isidoro <tartuzet@gmail.com>
2017-01-16 11:59:13 -0600

    tercera clase

--- index.js
+++ index.js
@@ -1,43 +1,4 @@
 $(function () {
-  //var header = $('header')
-  //console.log(header[0]);
-  //var h = $('#app-header').find('h1');
-  //console.log(h);
-  // var p = document.createElement('p');
-  // p.innerHTML = "Hola a todos !";
-  // document.body.appendChild(p);
-
-  // var a = $('<a>',{
-  //   href: 'http://platzi.com',
-  //   target: '_blank',
-  //   html: 'Ir a platzi'
-  // });
-  //
-  // $('#app-body').append(a[0]);
-  //
-  // //console.log(a.attr('href'));
-  // a.attr('href','https://google.com');
-  // a.html('ir a google');
-
-
-  var $h1 = $('h1').addClass('danger');
-
-  setTimeout(function(){
-      $h1.toggleClass('danger');
-  },1500)
-
-  // $('h1').css({
-  //   'font-size': '70px'
-  // })
-
-
-  $('header#app-header')
-  .append($('<p>',{html:'Me acaban de crear'}))
-
-console.log($('header#app-header')[0]);
-console.log($('#app-header')[0]);
-
-
 
 
 })
